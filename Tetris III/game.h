d9bfd03207e8a764305aa72db880a50630c9c561
Avinash Aryal <72702058+AvinashAryal@users.noreply.github.com>
2020-12-12 13:05:58 +0545

    Add files via upload

---
+++ Tetris III/game.h
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+#include<SDL2/SDL_ttf.h>
+#include<cstdlib>
+#include<ctime>
+
+class game
+{
+private:
+    bool b;
+    SDL_Window *w;
+    SDL_Renderer *r;
+    SDL_Surface *cyan_surf,*blue_surf,*orange_surf,*yellow_surf,*green_surf,*red_surf,*purple_surf;
+    SDL_Texture *cyan_tex,*blue_tex,*orange_tex,*yellow_tex,*green_tex,*red_tex,*purple_tex;
+    SDL_Surface *bg_surface,*score_surface;
+    SDL_Texture *bg_texture,*score_texture;
+    SDL_Color fontcolor = {255,0,0,255};
+    SDL_Rect score_rect = {0,0,50,20};
+    TTF_Font *score;
+    SDL_Event e;
+
+public:
+    void CreateGame();
+    bool GameSwitch();
+    bool NewGame();
+    void DisplayGame();
+    void DestroyGame();
+
+};
---
+++ Tetris III/main.cpp
@@ -0,0 +1,18 @@
+#include"game.h"
+#include"gamefield.h"
+#include"tetromino.h"
+
+game *g = new game;
+
+int main(int argc, char *argv[])
+{
+    g->CreateGame();
+    while(g->GameSwitch())
+    {
+        g->DisplayGame();
+    }
+    g->DestroyGame();
+    delete g;
+    return 0;
+}
+
